28966a462950cf7856e39fe49ad4b5d1435d6f51
Zakaria E Heddadi <zakaria.elheddadi@gmail.com>
2021-04-01 16:59:22 +0100

    update

--- config.js
+++ config.js
@@ -3,9 +3,8 @@ exports.HASURA_GRAPHQL_ENDPOINT =
 
 exports.PORT = process.env.PORT || 3000;
 exports.SMPT_AUTH_USER =
-  process.env.SMPT_AUTH_USER || "ecoletaiba.infos@gmail.com";
-exports.SMPT_AUTH_PASS =
-  process.env.SMPT_AUTH_PASS || "ecoletaiba.infos@ecoletaiba.infos";
+  process.env.SMPT_AUTH_USER || "ecoletaibaprivee@gmail.com";
+exports.SMPT_AUTH_PASS = process.env.SMPT_AUTH_PASS || "taiba@123@taiba";
 
 exports.FromEmail = process.env.FromEmail || "test@gmail.com";
 exports.DOMAINNAME = process.env.DOMAINNAME || "http://qrcode.ecoletaiba.com/";
